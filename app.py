9ade20dbcb26e6d5a3e9e06912021d5ed54f1f30
조민수 <chominsu10@gmail.com>
2022-01-22 23:10:25 +0900

    [ADD] connect front to back

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify,request
 import pymysql 
+from flask_cors import CORS
 
 def db_connector():
     db = pymysql.connect(host='127.0.0.1', user='root', password='12345678', charset='utf8',db='demo')
@@ -15,10 +16,14 @@ def db_connector():
 
 app = Flask(__name__)
 
+CORS(app)
 
-@app.route('/')
+
+@app.route('/',methods=["GET","POST"])
 def index():
-    return db_connector()
+    if request.method == 'GET':
+
+        return jsonify({'result':'success','data': db_connector(),'msg': '이 요청은 GET!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flask-test/src/App.js
+++ flask-test/src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const getUser = async () => {
+  const data = await axios.get("http://localhost:5000/");
+  console.log(data);
+  return data;
+};
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(getUser());
+  }, []);
+
+  console.log(users);
   return (
     <div className="App">
       <div>app</div>
